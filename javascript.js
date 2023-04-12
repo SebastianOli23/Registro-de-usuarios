f4aad11b0bc45808833606dadbc68783ea5f4d5e
SebastianOli23 <126100029+SebastianOli23@users.noreply.github.com>
2023-04-12 12:57:44 -0500

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,4 +1,37 @@
-let tabla=[    {        "Cedula": "123456789",        "IdUsuario": "001",        "Nombre": "Juan",        "Apellido": "Perez",        "Direccion": "Calle 123",        "Telefono": "555-1234",        "Correo": "juanperez@example.com",        "Fecha de Nacimiento": "2005-04-07"    },    {        "Cedula": "987654321",        "IdUsuario": "002",        "Nombre": "María",        "Apellido": "García",        "Direccion": "Avenida Principal",        "Telefono": "555-5678",        "Correo": "mariagarcia@example.com",        "Fecha de Nacimiento": "2001-08-02"    },    {        "Cedula": "456789123",        "IdUsuario": "003",        "Nombre": "Carlos",        "Apellido": "Rodríguez",        "Direccion": "Calle 456",        "Telefono": "555-2468",        "Correo": "carlosrodriguez@example.com",        "Fecha de Nacimiento": "2001-09-09"    }];
+let tabla=[    
+    {
+        "Cedula": "123456789",
+        "IdUsuario": "001",        
+        "Nombre": "Juan",
+        "Apellido": "Perez",       
+        "Direccion": "Calle 123",        
+        "Telefono": "555-1234",        
+        "Correo": "juanperez@example.com",        
+        "Fecha de Nacimiento": "2005-04-07"    
+    },   
+     
+    {        
+        "Cedula": "987654321",        
+        "IdUsuario": "002",        
+        "Nombre": "María",        
+        "Apellido": "García",        
+        "Direccion": "Avenida Principal",       
+         "Telefono": "555-5678",        
+         "Correo": "mariagarcia@example.com",        
+         "Fecha de Nacimiento": "2001-08-02"    
+    }, 
+       
+    {    
+        "Cedula": "456789123",        
+        "IdUsuario": "003",        
+        "Nombre": "Carlos",        
+        "Apellido": "Rodríguez",        
+        "Direccion": "Calle 456",        
+        "Telefono": "555-2468",        
+        "Correo": "carlosrodriguez@example.com",        
+        "Fecha de Nacimiento": "2001-09-09"    
+    }
+];
 
 let cuerpotabla=document.getElementById('tabla-usuarios')
 
@@ -30,6 +63,11 @@ function registrar(){
     let telefono=document.getElementById('telefono').value;
     let correo=document.getElementById('correo').value;
     let fechadenacimiento=document.getElementById('fechadenacimiento').value;
+    if (!nombre || !apellido || !direccion || !telefono || !correo || !idUsuario || !fechadenacimiento) {
+        alert('Por favor, llenar todos los campos obligatorios.');
+        return;
+    }
+    
 
     // Crear un objeto con los valores obtenidos
     let nuevoUsuario = {
@@ -42,6 +80,8 @@ function registrar(){
         "Correo": correo,
         "Fecha de Nacimiento": fechadenacimiento
     };
+
+    
 
     // Agregar el nuevo usuario al array
     tabla.push(nuevoUsuario);
@@ -58,8 +98,11 @@ function registrar(){
 
     // Mostrar un mensaje de éxito
     alert('Usuario registrado correctamente');
-}
-
-
-
     
+    let listado="";
+    for (let i = 0; i < tabla.length; i++) {
+        listado+=`<tr><th scope="row">${(i+1)}</th>
+        <td>${tabla[i].Cedula}</td><td>${tabla[i].IdUsuario}</td><td>${tabla[i].Nombre}</td><td>${tabla[i].Apellido}</td><td>${tabla[i].Direccion}</td><td>${tabla[i].Telefono}</td><td>${tabla[i].Correo}</td><td>${tabla[i]['Fecha de Nacimiento']}</td></tr>`
+    }
+    cuerpotabla.innerHTML=listado
+}
